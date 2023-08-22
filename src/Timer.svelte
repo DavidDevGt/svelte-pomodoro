ce2350896e5f5ec6e6ca5dcdc4eaabaa33d95234
Davidvl <josuedavidvl18@gmail.com>
2023-08-22 00:19:51 -0600

    first commit

---
+++ src/Timer.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { onMount } from 'svelte';
+
+    let minutos = 25;
+    let segundos = 0;
+    let corriendo = false;
+    let intervalo;
+
+    onMount(() => {
+        // Cargar el lofi yt
+        const audioElement = new Audio("URL_LOFI");
+        audioElement.loop = true;
+        audioElement.play();
+    });
+
+    const = iniciar = () => {
+        corriendo = true;
+        intervalo = setInterval(contar, 1000);
+    }
+
+    const detener = () => {
+        corriendo = false;
+        clearInterval(intervalo);
+    }
+
+    const resetear = () => {
+        detener();
+        minutos = 25;
+        segundos = 0;
+    }
+
+    const contar = () => {
+        if
+    }
+</script>
